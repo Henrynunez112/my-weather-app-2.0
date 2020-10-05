090575dd8f56ac050b1839fb10d43282b9f075be
Henrynunez112 <henrynunez@pursuit.org>
2020-10-05 17:02:58 -0400

    the real first push

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import "./App.css";
+
+function App() {
+  return <div className="App">
+    <h1>Hello World</h1>
+  </div>;
+}
+
+export default App;
