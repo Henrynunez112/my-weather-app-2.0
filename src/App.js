a838bce4c2e1b938b2b59a8d9c2887d12d946dae
Henrynunez112 <henrynunez@pursuit.org>
2020-10-05 17:09:39 -0400

    working on a api call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
+import Weather from "./Components/Weather"
 import "./App.css";
 
 function App() {
   return <div className="App">
-    <h1>Hello World</h1>
+    <Weather />
   </div>;
 }
 
