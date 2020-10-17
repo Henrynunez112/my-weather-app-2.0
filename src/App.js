ccad709e526ad8a6cc0658bb0c4cf4629026ad1e
Henrynunez112 <henrynunez@pursuit.org>
2020-10-17 14:02:04 -0400

    attempting to create the website from scratch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
-import Weather from "./Components/Weather"
+import Weather from "./Components/Weather";
+import Home from "./Components/Home/Home";
 import "./App.css";
 
 function App() {
   return <div className="App">
-    <Weather />
+    <Home />
+
+    {/* <Weather /> */}
   </div>;
 }
 
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../Home/Home.css"
+
+const Home = () =>{
+    return(
+        <div className="homeContainer container">
+            <div className="homeHeader">
+                <h1>Hello World</h1>
+            </div>
+        </div>
+    )
+
+};
+export default Home;
