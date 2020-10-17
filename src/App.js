43ae4c5e897da62a575ceec4dfe70cb20b48c28a
Henrynunez112 <henrynunez@pursuit.org>
2020-10-17 14:44:20 -0400

    creating a navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import Weather from "./Components/Weather";
+import NavBar from "./Components/NavBar/NavBar";
 import Home from "./Components/Home/Home";
 import "./App.css";
 
 function App() {
-  return <div className="App">
-    <Home />
-
-    {/* <Weather /> */}
-  </div>;
+  return (
+    <div className="App">
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/weather">
+          <Weather />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Henry's Widget App
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <NavLink className="nav-link" exact to="/">
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" exact to="/weather">Weather</NavLink>
+          </li>
+          {/* 
+          <li className="nav-item">
+            <NavLink className="nav-link">
+              Pricing
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              className="nav-link disabled"
+              href="#"
+              tabindex="-1"
+              aria-disabled="true"
+            >
+              Disabled
+            </NavLink>
+          </li> */}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+export default NavBar;
