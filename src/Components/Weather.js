e2e6d44fd19d76535e394fe23c3fa2c86f6696de
Henrynunez112 <henrynunez@pursuit.org>
2020-10-21 17:05:35 -0400

    setting the promise for axios call

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,12 +1,20 @@
-import React, {useEffect, useState} from 'react';
-const {WEATHER_API_KEY} = process.env;
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+const { WEATHER_API_KEY } = process.env;
 
-const Weather = () =>{
-    return(
-        <div>
-            Hello World
-        </div>
-    )
+const Weather = () => {
+  const [weather, setWeather] = useState({});
+  // const [isLoading, setIsLoading] = useState(true);
 
-}
+  //created a promise call to get the longitude and latitude
+  const getLocation = async () => {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        resolve([position.coords.latitude, position.coords.latitude]);
+      });
+    });
+  };
+
+  return <div></div>;
+};
 export default Weather;
