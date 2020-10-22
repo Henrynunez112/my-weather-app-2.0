0ae19492bb71e4f4ea5c27c4b68d2e45d09903af
Henrynunez112 <henrynunez@pursuit.org>
2020-10-21 23:26:56 -0400

    api call made with dotenv

--- src/Components/WeatherComponents/Weather.js
+++ src/Components/WeatherComponents/Weather.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from "react";
 import WeatherBody from "../WeatherComponents/WeatherBody";
 import axios from "axios";
-require('dotenv').config()
-// const { WEATHER_API_KEY } = process.env;
 
 const Weather = () => {
   const [weather, setWeather] = useState({});
+  console.log(process.env.REACT_APP_API_KEY);
   // const [isLoading, setIsLoading] = useState(true);
+  const key = process.env.REACT_APP_API_KEY;
 
   //created a promise call to get the longitude and latitude
   const getLocation = async () => {
     return new Promise((resolve, reject) => {
-      debugger
+      debugger;
       navigator.geolocation.getCurrentPosition((position) => {
         resolve([position.coords.latitude, position.coords.longitude]);
       });
@@ -21,9 +21,9 @@ const Weather = () => {
   const weatherCall = async (lat, lon) => {
     // const API_URL = "api.openweathermap.org/data/2.5/forecast";
     // const URL = API_URL + ``
-    debugger
+    debugger;
     let res = await axios.get(
-      `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}`
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${key}`
     );
     return res.data;
   };
