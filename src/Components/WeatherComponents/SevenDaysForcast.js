ff54ca816e97de946cefd622d1a1fd02e8233d7f
Henrynunez112 <henrynunez@pursuit.org>
2020-10-27 21:23:31 -0400

    got the 7 day forcast to appear

--- src/Components/WeatherComponents/SevenDaysForcast.js
+++ src/Components/WeatherComponents/SevenDaysForcast.js
@@ -7,13 +7,14 @@ const SevenDaysForcast = ({currentWeather}) =>{
     return(
         <div>  
             <ul>
-                {forcasts.map((forcast) =>{
+                {forcasts.map((forcast, i) =>{
                     debugger
                    
                         return(
-                            <div>
+                            <div key={i + 1}>
                                 <li>
                                     <p>{moment(forcast.datetime).format('MMMM Do YYYY')}</p>
+                                    <img className="forcastImg" alt="seven day forcast" src={`https://www.weatherbit.io/static/img/icons/${forcast.weather.icon}.png`} />
                                     <p>High: {forcast.high_temp}°F</p>
       <p>Low: {forcast.low_temp}°F</p>
                                 </li>
