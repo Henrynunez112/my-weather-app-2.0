a42c0bcdd098b6aed983339335cb0b09fb6cc7b2
Henrynunez112 <henrynunez@pursuit.org>
2020-11-11 11:20:00 -0500

    created a border with weatherBody

--- src/Components/WeatherComponents/WeatherBody.js
+++ src/Components/WeatherComponents/WeatherBody.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import WeatherHeader from './WeatherHeader';
 import WeatherDay from './WeatherDay';
+import './WeatherBody.css';
 // import SevenDayForcast from './SevenDaysForcast';
 
 const WeatherBody = ({current, weather, sunMoon, currentWeatherImg}) =>{
     debugger
     return (
         <div className='weatherBody'>
-            
             <WeatherHeader current={current} currentWeatherImg={currentWeatherImg} />
             <WeatherDay weather={weather} sunMoon={sunMoon} />
         </div>
