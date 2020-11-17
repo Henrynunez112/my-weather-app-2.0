6c2e9fc3336d3009f413271dc25063211c9a7a5d
Henrynunez112 <henrynunez@pursuit.org>
2020-11-17 16:29:47 -0500

    css flex-direction column

--- src/Components/WeatherComponents/Forecast.js
+++ src/Components/WeatherComponents/Forecast.js
@@ -1,15 +1,33 @@
 import React from 'react';
+import Moment from 'react-moment';
 
 const Forcast = ({forecast}) =>{
     return forecast.map((day, i) =>{
         return (
             <div key={i} className="forecast container">
-                <div>
+                <div className="forcastImg">
                 <img
             alt=""
             className="sevenDayImg"
             src={`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
           />
+                </div>
+                <div key={i + 1} className="">
+                <Moment format="dddd" unix>
+              {day.dt}
+            </Moment>
+          <p id="dayP">{day.weather[0].main}</p>
+
+
+                </div>
+                <div key={i + 2} className="">
+                <p id="degrees">
+            {" "}
+            <span id="degSpan">
+              {day.temp.min.toString().slice(0, 2)}°
+            </span> | {day.temp.max.toString().slice(0, 2)}°
+          </p>
+
                 </div>
 
             </div>
--- src/Components/WeatherComponents/WeatherBody.js
+++ src/Components/WeatherComponents/WeatherBody.js
@@ -6,18 +6,16 @@ import './WeatherBody.css';
 
 const WeatherBody = ({current, weather, sunMoon, currentWeatherImg, forecast}) =>{
     return (
-        <div className='weatherBody jumbotron container'>
-            <div className='col-sm-3 col-md-auto col-lg-6'>
-            <div className='row align-items-start justify-content-start'>
+        <div className='weatherBody container'>
+            <div className="row">
             <WeatherHeader current={current} currentWeatherImg={currentWeatherImg} />
-            </div>
-            <div className='row align-items-start justify-content-start'>
             <WeatherDay weather={weather} sunMoon={sunMoon} />
             </div>
-            </div>
-            <div className="col-sm-9">
+            <div className="row">
             <Forecast forecast={forecast} />
             </div>
+    
+         
         </div>
     )
 
