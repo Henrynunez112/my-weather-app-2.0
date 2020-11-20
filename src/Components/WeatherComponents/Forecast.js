8e6bb40c2cc28e3741ceedaa64ce45afd0a884f5
Henrynunez112 <henrynunez@pursuit.org>
2020-11-19 20:38:55 -0500

    cleaned up some css

--- src/Components/WeatherComponents/Forecast.js
+++ src/Components/WeatherComponents/Forecast.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import Moment from 'react-moment';
-// import './Forecast.css';
+import './Forecast.css';
 
 const Forcast = ({forecast}) =>{
     return forecast.map((day, i) =>{
         return (
-            <div key={i} className="forecast container col">
-                <div className="forcastImg">
+            <div key={i} className="forecast col">
+
+                <div className="forecastImg row">
                 <img
             alt=""
             className="sevenDayImg"
             src={`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
           />
                 </div>
-                <div key={i + 1} className="">
+
+                <div key={i + 1} className="row forcastDay">
+                  <div className="col dayOfWeek">
                 <Moment format="dddd" unix>
               {day.dt}
             </Moment>
+                  </div>
+                  <div className="col dayDescription">
           <p id="dayP">{day.weather[0].main}</p>
+                  </div>
 
 
                 </div>
-                <div key={i + 2} className="">
+                <div key={i + 2} className="row">
                 <p id="degrees">
             {" "}
             <span id="degSpan">
