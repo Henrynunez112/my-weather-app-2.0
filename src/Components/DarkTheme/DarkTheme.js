31b4fbfd3ed223f121253bdaaeac49f7563f4720
Henrynunez112 <henrynunez@pursuit.org>
2020-12-03 12:19:05 -0500

    cleaning a little more css

--- src/Components/DarkTheme/DarkTheme.js
+++ src/Components/DarkTheme/DarkTheme.js
@@ -10,6 +10,5 @@ export const GlobalStyle = createGlobalStyle`
 body{
     background: ${({ theme }) => theme.body} !important;
     color: ${({ theme }) => theme.text} !important;
-    border: ${({ theme }) => theme.border} !important;
     transition: all 0.25s linear;
 }`
