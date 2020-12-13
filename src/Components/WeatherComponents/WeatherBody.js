82c557d7d3772ecd0b7c1ccb3565ffe463f61d3b
Henrynunez112 <henrynunez@pursuit.org>
2020-12-12 20:24:14 -0500

    cleaned up files and created background gradient for WeatherBody

--- src/Components/WeatherComponents/WeatherBody.js
+++ src/Components/WeatherComponents/WeatherBody.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import clearSkies from '../Images/clearSkies.svg';
 import WeatherHeader from './WeatherHeader';
 import WeatherDay from './WeatherDay';
 import Forecast from './Forecast';
 import './WeatherBody.css';
 
 const WeatherBody = ({ current, weather, sunMoon, currentWeatherImg, forecast, theme }) => {
+    console.log(currentWeatherImg)
     const [currentWeather, setCurrentWeather] = useState("")
     let backgroundColor = '';
-    console.log(currentWeather)
-
+    let textColor = ''
 
     useEffect(() => {
         setCurrentWeather(currentWeatherImg.description)
@@ -17,34 +18,40 @@ const WeatherBody = ({ current, weather, sunMoon, currentWeatherImg, forecast, t
 
     switch (currentWeather) {
         case 'mist':
-            backgroundColor = 'purple';
-            break;
-        case 'snow':
-        case 'broken clouds':
-            backgroundColor = 'grey';
+            backgroundColor = 'to bottom left, #355070, #6d597a, #b56576, #e56b6f, #eaac8b';
+            textColor = "#E2E2E2";
             break;
         case 'thunderstorm':
-            backgroundColor = 'blue';
+            backgroundColor = '#432371, #FAAE7B';
+            textColor = "#E2E2E2";
             break;
+        case 'snow':
         case 'rain':
         case 'shower rain':
-            backgroundColor = 'green';
+            backgroundColor = 'to bottom left, #EBF4F5, #B5C6EO';
+            textColor = '#363537';
             break;
         case 'clear sky':
         case 'few clouds':
+        case 'broken clouds':
         case 'scattered clouds':
-            backgroundColor = 'yellow';
+            backgroundColor = 'to bottom left, #5465ff, #788bff, #9bb1ff, #bfd7ff, #e2fdff';
+            textColor = '#363537';
             break;
         default:
             backgroundColor = 'black';
+            textColor = '#E2E2E2';
 
     }
 
     const darkModeWeatherBody = {
         border: '3px solid #f0883e',
         backgroundSize: 'cover',
-        backgroundColor: `${backgroundColor}`
+        backgroundImage: `linear-gradient(${backgroundColor})`,
+        color: `${textColor}`
     }
+
+
     return (
         <div className='weatherBody container' style={theme === 'dark' ? darkModeWeatherBody : null}>
             <div className="row currentWeather">
